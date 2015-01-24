77980276fbdecd8b616782ab5fc4e278d7d6a391
Brian Naughton <bpnaught@ncsu.edu>
2015-01-23 20:48:28 -0500

    hw1 part c done

--- hw1.sh
+++ hw1.sh
@@ -8,7 +8,9 @@ awk 'END { print NR }' /home/st790_003/hw01/merge-geno.bim
 echo 'How many SNPs on each chromosome?'
 awk '{print $1}' /home/st790_003/hw01/merge-geno.bim | uniq -c
 # c
-
+echo 'How many SNPs are on the MAP4 gene?'
+awk '$1==3 && $4>=47892180 && $4<=48130769  {print $0}'\
+ /home/st790_003/hw01/merge-geno.bim | wc -l
 # d.i
 
 # d.ii
